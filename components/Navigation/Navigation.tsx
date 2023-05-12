fc08f40bcceb50859879272fdc6f4d16282ba56f
keerthangopi <keerthan44@gmail.com>
2023-05-12 12:09:02 +0530

    First Commit

---
+++ components/Navigation/Navigation.tsx
@@ -0,0 +1,60 @@
+import * as React from "react";
+import { motion } from "framer-motion";
+import { MenuToggle } from "./MenuToggle";
+import { MobileNavigation } from "./MobileNavigation";
+import Link from "next/link";
+
+const variants = {
+  open: {
+    left: 0,
+    pointerEvent: "none",
+    transition: {
+      duration: 1,
+      ease: [0.6, 0.01, -0.05, 0.9],
+    },
+  },
+  closed: {
+    left: "-100vw",
+    pointerEvent: "none",
+    transition: {
+      delay: 1,
+      duration: 1,
+      ease: [0.6, 0.01, -0.05, 0.9],
+    },
+  },
+};
+
+export const Navigation = ({ isOpen, toggleOpen }: any) => {
+  return (
+    <motion.div
+      initial={false}
+      animate={isOpen ? "open" : "closed"}
+      className="navigation-wrapper"
+    >
+      <MobileNavigation variants={variants} isOpen={isOpen} />
+      <div
+        data-scroll
+        data-scroll-sticky
+        data-scroll-target="#menu-target"
+        className="menu-top"
+      >
+        <Link href="/">
+          <a className="brand-logo">
+            <img
+              className="brand-logo__icon"
+              src="svg/KG.svg"
+              alt="adeola logo icon"
+            />
+            <span className="brand-logo__text-wrapper">
+              <img
+                className="brand-logo__text"
+                src="svg/try3.svg"
+                alt="adeola logo text"
+              />
+            </span>
+          </a>
+        </Link>
+      </div>
+    </motion.div>
+  );
+};
